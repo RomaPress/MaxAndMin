ee56608c8161405a21fd6025602d519386a7a0c6
timstuf <52314464+timstuf@users.noreply.github.com>
2020-01-26 11:30:34 +0200

    improved smth, removed the i==0

--- src/main/java/com/press/MaxAndMin.java
+++ src/main/java/com/press/MaxAndMin.java
@@ -5,23 +5,14 @@ class MaxAndMin {
     public int[] maxmin(int[] mass) {
         if (mass == null) throw new NullPointerException();
         if(mass.length == 0){
-            int[] extremum = new int[0];
-            return extremum;
+            return new int[0];
         }
-        int max = 0, min = 0;
+        int max = mass[i], min = mass[i];
         for (int i = 0; i < mass.length; i++) {
-            if (i == 0) {
-                min = mass[i];
-                max = mass[i];
-            }
             if (mass[i] < min) min = mass[i];
             if (mass[i] > max) max = mass[i];
         }
-        int[] extremum = new int[2];
-        extremum[0] = min;
-        extremum[1] = max;
-        System.out.println(extremum[0]);
-        System.out.println(extremum[1]);
+        int[] extremum = new int[2]{min, max};
         return extremum;
     }
 }
